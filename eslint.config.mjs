2d2ae91c1d97ca71cb585cd34f4bbfbe4d82cc02
JeremyFerreiraDeSousa <jeremy.ferreira@topcompare.be>
2025-12-14 17:02:52 +0100

    initial app with configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,55 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettierPlugin from 'eslint-plugin-prettier'
+// Import plugins directly
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
 
-export default eslintConfig;
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      // General
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'comma-dangle': ['error', 'never'],
+      'prettier/prettier': ['error'],
+
+      // Import sorting
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^type'], ['^react$', '^next', '^next/.*', '^']]
+        }
+      ],
+      'simple-import-sort/exports': 'error',
+
+      // TypeScript
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+      ],
+
+      // Unused imports
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+      ]
+    }
+  }
+])
